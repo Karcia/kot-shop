aca2e59853feccde191e575acc2c3c505cb89aee
Mateusz <kroolikk@example.com>
2014-05-14 11:40:07 +0200

    improved admin controller

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,41 +1,37 @@
 # -*- encoding: utf-8 -*-
 class AdminController < ApplicationController
 
+  before_filter :check_admin, :except => [:login, :enter]
 
-before_filter :check_admin, :except => [:login, :enter]
 
+  def login
+    session[:admin] == false
+  end
 
-def login
-  session[:admin] = true
-  redirect_to admin_path
-end
+  def index
+  end
 
-def index
-end
+  def enter
+    if params[:login] == "karcia" && params[:password] == "kotek"
+      session[:admin] == true
+      redirect_to admin_path
+    else
+      flash[:alert] = "Niepoprawne dane."
+      redirect_to admin_login_path
+    end
+  end
 
-def enter
-  if params[:login] == "karcia" && params[:password] == "kotek"
-    session[:admin] == true
-    session[:account_type] = "Admin"
-    redirect_to admin_path
-  else
-    flash[:alert] = "Niepoprawne dane."
+  def logout
+    session[:admin] = false
     redirect_to admin_login_path
   end
-end
 
-def logout
-  session[:logout_requested] = true
-  session[:admin] = false
-  redirect_to admin_login_path
-end
+  protected
 
-protected
-
-def check_admin  
-  if session[:admin] != true
-    redirect_to admin_login_path
+  def check_admin  
+    if session[:admin] != true
+      redirect_to admin_login_path
+    end
   end
-end
 
 end
