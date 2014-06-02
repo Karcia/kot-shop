7a5d755f6d42253f17d8ee4409a703f45d6cf273
Karcia <karciawow@gmail.com>
2014-06-02 18:10:00 +0200

    Doing front_product view

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,16 @@
 class Product < ActiveRecord::Base
   attr_protected :id
-  #attr_accessible :description, :image, :name, :avatar, :image_cache
 
   mount_uploader :image, ImageUploader
-end
 
+  def self.recreate_all_images 
+    Product.all.each do |p|
+      if p.image.present?
+        p.image.cache_stored_file!
+        p.image.recreate_versions!
+        puts "recreating photo #{p.id}: #{p.save}"
+      end
+    end
+    return 'done'
+  end
+end
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -40,6 +40,9 @@ class ImageUploader < CarrierWave::Uploader::Base
     process :resize_to_fill => [90, 58]
   end
 
+  version :thumb_1 do
+    process :resize_to_fill => [500, 350]
+  end
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   # def extension_white_list
